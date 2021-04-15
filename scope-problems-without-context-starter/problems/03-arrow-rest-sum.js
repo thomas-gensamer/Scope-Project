a60eba85757eb287ec14cb4e0205e62b302e8c34
Ash <thomas.gensamer@gmail.com>
2021-04-15 17:31:06 -0400

    solve restsum

--- scope-problems-without-context-starter/problems/03-arrow-rest-sum.js
+++ scope-problems-without-context-starter/problems/03-arrow-rest-sum.js
@@ -8,9 +8,28 @@ Examples:
 arrowRestSum(3,5,6); // => 14
 arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 14
 arrowRestSum(0); // => 0
-***********************************************************************/
 
-// your code here!
+understand
+write an arrow function that take params of ...num
+interate through num array using .reduce
+return iterated el summed together
+***********************************************************************/
+// plan
+// declare arrow func named arrowRestSum that takes in ...num
+// set sum = to 0
+// set num.reduce thats take (accum, el)
+    // set sum = acuum + el
+// return sum
+
+const arrowRestSum = (...num) => {
+  let sum = 0;
+  num.reduce(function (accum, el) {
+    sum = accum + el;
+    return sum;
+  })
+return sum;
+}
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
