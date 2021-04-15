5b041cccc29b45c88a92c1c27bc04316f817ddab
Chrisbh4 <chrismbh4@gmail.com>
2021-04-15 14:54:21 -0700

    solved 04

--- scope-problems-without-context-starter/problems/04-arrow-average-value.js
+++ scope-problems-without-context-starter/problems/04-arrow-average-value.js
@@ -9,8 +9,33 @@ arrowAvgValue([10, 20]); // => 15
 arrowAvgValue([2, 3, 7]); // => 4
 arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
+/*
+Understand
+declare a with const func in arrow syntax called arrowAvgvalue and takes in an array as the parameter
+wants to return the average of the total sum of the array
 
+Plan
+const arrowAvgValue with arr as the params
+let sum  = declare arr.reduce with acum ,  ele
+  acum + el
+  return acum
+
+return sum / arr.length
+*/
+const arrowAvgValue = arr =>{
+  let sum = arr.reduce(function( acum , el){
+      return acum + el;
+      //return acum
+  })
+console.log(sum)
+  return sum/ arr.length
+}
 // your code here!
+console.log(arrowAvgValue([10, 20]))
+console.log(arrowAvgValue([2, 3, 7]))
+console.log(arrowAvgValue([100, 60, 64]))
+
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
