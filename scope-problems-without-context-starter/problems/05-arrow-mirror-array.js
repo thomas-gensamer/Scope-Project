a1b48993dd9b3df3c226ed94b82a11b1a4b5fd45
Ash <thomas.gensamer@gmail.com>
2021-04-15 18:20:49 -0400

    solved mirrorarray

--- scope-problems-without-context-starter/problems/05-arrow-mirror-array.js
+++ scope-problems-without-context-starter/problems/05-arrow-mirror-array.js
@@ -8,7 +8,25 @@ Examples:
 
 arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
 arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
+
+understanding
+wants arrow function called arrowMirrorArray that takes in an array as a param
+return a newArray with the original array mirrored
 ***********************************************************************/
+/*
+declare arrowMirrorArray with arr as argument
+set variable of copy to arr.slice
+set variable of words to copy.split
+set variable rev to words.reverse
+return arr.concat(rev)
+*/
+
+const arrowMirrorArray = arr => {
+  let copy = arr.slice();
+  let words = copy.reverse();
+
+  return arr.concat(words);
+}
 
 // your code here
 
